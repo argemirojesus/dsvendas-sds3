4a73d37fb47eb768b6e34bde65187959adac7b4b
argemirojesus <argemiro.jesus@gmail.com>
2021-05-03 23:32:56 -0300

    Project created

---
+++ backend/src/test/java/br/com/miroviski/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miroviski.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
